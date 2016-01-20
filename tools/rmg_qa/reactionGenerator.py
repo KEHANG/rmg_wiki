d790dbda245dacf226701fee7d8d55894d345877
KEHANG <kehanghan@gmail.com>
2016-01-19 20:59:45 -0500

    standardize the pathway generation process
    - provide a input file
    
    - get chemkin files at the end

--- tools/rmg_qa/reactionGenerator.py
+++ tools/rmg_qa/reactionGenerator.py
@@ -3,20 +3,29 @@ from rmgpy.rmg.model import getFamily
 from rmgpy.kinetics import KineticsData
 from rmgpy.data.kinetics.family import TemplateReaction
 
-def pathwayGenerator(rxtA, rxtB, pdtC, rmg, only_families):
+def pathwayGenerator(reactants, products, rmg, only_families):
 	genRxn = database.kinetics.generateReactionsFromFamilies
 
 	# LOOP starts
 	newReactions = []
-	for moleculeA in rxtA.molecule:
-	    for moleculeB in rxtB.molecule:
-	        for moleculeC in pdtC.molecule:
-	            newReactions.extend(genRxn(reactants=[moleculeA, moleculeB],\
-	            products=None, only_families=only_families))
+	rxt_mol_mutation_num = 1
+	pdt_mol_mutation_num = 1
+	for reactant in reactants:
+		rxt_mol_mutation_num *= len(reactant.molecule)
+	for product in products:
+		pdt_mol_mutation_num *= len(product.molecule)
+
+	for mutation_i in range(rxt_mol_mutation_num):
+		rxts_mol = [spc.molecule[mutation_i%(len(spc.molecule))] for spc in reactants]
+		for mutation_j in range(pdt_mol_mutation_num):
+			pdts_mol = [spc.molecule[mutation_j%(len(spc.molecule))] for spc in products]
+
+	        newReactions.extend(genRxn(reactants=rxts_mol,\
+	            products=pdts_mol, only_families=only_families))
 
 	rmg.reactionModel.newSpeciesList = []
 	rmg.reactionModel.newReactionList = []
-	rmg.reactionModel.processNewReactions(newReactions, rxtA)
+	rmg.reactionModel.processNewReactions(newReactions, reactants)
 
 	# gen thermo and transport for new species
 	for spec in rmg.reactionModel.newSpeciesList:
@@ -57,3 +66,31 @@ def pathwayGenerator(rxtA, rxtB, pdtC, rmg, only_families):
 	        # If this is going to be run through pressure dependence code,
 	        # we need to make sure the barrier is positive.
 	        reaction.fixBarrierHeight(forcePositive=True)
+
+def loadInput(filename):
+	reactants_list = []
+	products_list = []
+	family_list = []
+	with open(filename, 'rb') as input_file:
+		lines = input_file.readlines()
+		for line in lines:
+			if line != '\n' and not line.startswith("!"):
+				if 'PATHWAY' in line:
+					pass
+				elif 'END' in line:
+					break
+				else:
+					rhs = line.strip().split("==")[0]
+					lhs = line.strip().split("==")[1]
+
+					family = rhs.strip().split(":")[0]
+					reactants_tokens = rhs.strip().split(":")[1].split()
+					products_tokens = lhs.strip().split()
+
+					reactants = filter(lambda item:item!='+', reactants_tokens)
+					products = filter(lambda item:item!='+', products_tokens)
+
+					family_list.append(family)
+					reactants_list.append(reactants)
+					products_list.append(products)
+	return family_list, reactants_list, products_list
